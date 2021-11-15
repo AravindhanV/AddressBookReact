4ba46d938bef89d860c5d210585e369d7c482396
Aravindhan V <aravindhan.v@perfios.com>
2021-11-15 18:03:05 +0530

    ADDED: Main header for website

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div class="main-content">
+                <div class="header-content">
+                    <div class="addr-detail-text">
+                        Person Details
+                        <div class="addr-count">0</div>
+                    </div>
+                    <a href="../pages/contact_form.html" class="add-button">
+                        <img src="../assets/add_contact.png" alt="" />Add Person</a
+                    >
+                </div>
+                <div class="table-main">
+                    <table id="table-display" class="table"></table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
